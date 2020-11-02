339e70692b7a0430d28a6101f10878a1fe98445f
Kevin-DDR <kevindoninderosiere@gmail.com>
2020-11-02 11:15:03 +0100

    Added literals parsing

--- projet.js
+++ projet.js
@@ -9,39 +9,48 @@ const rl = readline.createInterface({
 class Requete{
   constructor(requete) {
     let words = requete.split(" ");
+    
+
+    
+    
     this.sujet = words[0]
     this.predicat = words[1]
-    this.objet = words[2]
 
-    if(words[0].match("^\\?.*")){
+    let tmp = words;
+    console.log(words)
+    tmp.shift();
+    tmp.shift();
+    this.objet = tmp.join(" ");
+
+    if(this.sujet.match("^\\?.*")){
       console.log("Sujet est une wildcard");
       this.wildSujet = true;
-      if(words[0].match("^\\?@.*")){
+      if(this.sujet.match("^\\?@.*")){
         console.log("Sujet a une annotation")
         this.hasAnnoSujet = true;
-        let tmp = words[0].split("@")
+        let tmp = this.sujet.split("@")
         this.annoSujet = tmp[1]
       }
     }
 
-    if(words[1].match("^\\?.*")){
+    if(this.predicat.match("^\\?.*")){
       console.log("Predicat est une wildcard");
       this.wildPredicat = true;
-      if(words[1].match("^\\?@.*")){
+      if(this.predicat.match("^\\?@.*")){
         console.log("Predicat a une annotation")
         this.hasAnnoPredicat = true;
-        let tmp = words[1].split("@")
+        let tmp = this.predicat.split("@")
         this.annoPredicat = tmp[1]
       }
     }
 
-    if(words[2].match("^\\?.*")){
+    if(this.objet.match("^\\?.*")){
       console.log("Objet est une wildcard");
       this.wildObjet = true;
-      if(words[2].match("^\\?@.*")){
+      if(this.objet.match("^\\?@.*")){
         console.log("Objet a une annotation")
         this.hasAnnoObjet = true;
-        let tmp = words[2].split("@")
+        let tmp = this.objet.split("@")
         this.annoObjet = tmp[1]
       }
     }
@@ -61,13 +70,42 @@ function openFile(path, requete){
     const lines = data.split(/\r?\n/);
     lines.forEach((line) => {
       let printLine = true;
-      
+
       words = line.split(" ");
+
+      
+      let sujet = words[0]
+      let predicat = words[1]
+      
+      
+      tmp = words;
+      tmp.shift();
+      tmp.shift();
+      tmp.pop();
+      let objet = tmp.join(" ");
+
+
       if (requete.wildSujet && requete.hasAnnoSujet){
-        if (!words[0].match(".*@"+requete.annoSujet+"$")){
+        if (!sujet.match(".*@"+requete.annoSujet+"$")){
           printLine = false;
         }
-      }else if(!requete.wildSujet &&  words[0] != requete.sujet){
+      }else if(!requete.wildSujet &&  sujet != requete.sujet){
+        printLine = false;
+      }
+
+      if (requete.wildPredicat && requete.hasAnnoPredicat){
+        if (!predicat.match(".*@"+requete.annoPredicat+"$")){
+          printLine = false;
+        }
+      }else if(!requete.wildPredicat &&  predicat != requete.predicat){
+        printLine = false;
+      }
+
+      if (requete.wildObjet && requete.hasAnnoObjet){
+        if (!objet.match(".*@"+requete.annoObjet+"$")){
+          printLine = false;
+        }
+      }else if(!requete.wildObjet &&  objet != requete.objet){
         printLine = false;
       }
 
